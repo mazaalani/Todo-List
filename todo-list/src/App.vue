c97c67a7c9907bf65966114651766740bcc93d5e
mazaalani <72396085+mazaalani@users.noreply.github.com>
2021-08-13 14:23:14 -0400

    Initial commit

---
+++ todo-list/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <TodoBoard></TodoBoard>
+  </div>
+</template>
+
+<script>
+import TodoBoard from "./components/TodoBoard.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoBoard,
+  },
+};
+</script>
+
+<style></style>
---
+++ todo-list/src/components/TodoBoard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>Rentrez vos choses à faire.</h1>
+
+    <form>
+      <div class="form-group">
+        <label for="action">Action</label>
+        <input v-model="action" type="text" id="action" class="form-control" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoBoard",
+  data() {
+    return {
+      action: "",
+    };
+  },
+};
+</script>
+
+<style></style>
